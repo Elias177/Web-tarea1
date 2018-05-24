38b485fa08523ef1553a7393be7588ed1a75daae
Elias Santiago <eliassantiago177@gmail.com>
2018-05-24 05:50:27 -0400

    Puntos faltantes

--- src/main/java/logical/Main.java
+++ src/main/java/logical/Main.java
@@ -1,11 +1,10 @@
 package logical;
 
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -17,7 +16,9 @@ public class Main {
         String url = "https://www." + scanner.nextLine();
         Document page = Jsoup.connect(url).get();
 
+        //a) Indicar la cantidad de lineas del recurso retornado.
         int lineas = page.html().split("\n").length;
+        //b) Indicar la cantidad de párrafos (p) que contiene el documento HTML.
         int parrafos = page.getElementsByTag("p").size();
 
         System.out.println("A) Cantidad de lineas: " + lineas + "\n");
@@ -26,37 +27,95 @@ public class Main {
         pause(scanner);
         System.out.println("C) Cantidad de imagenes en los parrafos: " + puntoC(page) + "\n");
         pause(scanner);
-        System.out.println("D) Ccantidad de form: \n\t-Metodo GET: " + puntoDget(page) + "\n\t-Metodo POST: " + puntoDpost(page) + "\n");
+        System.out.println("D) Cantidad de form: \n\t-Metodo GET: " + puntoDget(page) + "\n\t-Metodo POST: " + puntoDpost(page) + "\n");
         pause(scanner);
-
+        System.out.println("E)Form y sus inputs con su tipo:\n");
+        puntoE(page);
+        pause(scanner);
+        System.out.println("\n");
+        puntoF(page,url);
     }
 
-    public static void pause(Scanner scanner){
+    private static void pause(Scanner scanner){
         System.out.println("Presione enter para continuar...");
         String p = scanner.nextLine();
     }
-    public static int puntoC(Document d){
+    private static int puntoC(Document d){
         int imagenes = 0;
         for(Element e : d.getElementsByTag("p")){
-           imagenes =  e.getElementsByTag("img").size();
+
+           if(e.toString().contains("<img"))
+               imagenes++;
+
             }
 
         return imagenes;
     }
-    public static int puntoDpost(Document d){
+
+    private static int puntoDpost(Document d){
         int post = 0;
         for(Element e : d.getElementsByTag("form")){
-            e.getElementsByAttributeValue("method","post").size();
+            for(Element e2 : d.getElementsByAttributeValue("method","post")){
+                post++;
+            }
         }
         return post;
     }
 
-    public static int puntoDget(Document d){
+    private static int puntoDget(Document d){
         int get = 0;
         for(Element e : d.getElementsByTag("form")){
-            e.getElementsByAttributeValue("method","get").size();
+            for(Element e2 : d.getElementsByAttributeValue("method","get")){
+                get++;
+            }
         }
         return get;
     }
 
+    private static void puntoE(Document d) {
+        String id = " ";
+        for (Element e1 : d.getElementsByTag("form")) {
+
+            if(e1.attributes().get("id").equalsIgnoreCase("  ")){
+                id = "-el form no tiene id-";
+            }else{
+                id= e1.attributes().get("id");
+            }
+            System.out.println("El form " + id + " tiene input de tipo: " +"\n");
+
+            for (Element e : d.getElementsByTag("input")) {
+
+                System.out.println(e.attributes().get("type"));
+            }
+
+        }
+
+    }
+
+    private static void puntoF(Document d, String url) throws IOException {
+        Element eP = d.createElement("inte");
+        for (Element e1 : d.getElementsByTag("form")) {
+
+            for(Element e2 : d.getElementsByAttributeValue("method","post")){
+
+                eP = e2;
+            }
+
+
+        }
+
+
+        Connection.Response response =
+                Jsoup.connect(url)
+                        .data("asignatura","practica1")
+                        .header("matricula","2014-0717")
+                        .method(Connection.Method.POST)
+                        .execute();
+
+        //parse the document from response
+        System.out.println(response.parse());
+        System.out.println(response.statusMessage());
+        System.out.println(response.statusCode());
+
+    }
 }
